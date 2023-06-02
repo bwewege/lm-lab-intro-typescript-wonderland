4dd3c632d2a6be55878fd39b9daef9ca76f526fd
Brian <brian@vvgonline.com>
2023-06-02 19:40:47 +0100

    Feat - Choose random game if PS4 is chosen - chapter7

--- src/chapter_7/chapter_7_whats_next.ts
+++ src/chapter_7/chapter_7_whats_next.ts
@@ -1,32 +1,52 @@
-import { endAdventure } from "../..";
+import { haveAdventures } from "../..";
 import { clear, print, askQuestion } from "../ui/console";
 import { parseConsoleInput } from "../ui/parse_input";
 
 export function whatsNext(): void {
   clear(false);
-  print("------------------------");
-  print(`Whats are you going to do now?`);
-  print("------------------------");
-  print("You can see a number of consoles: ");
+  print("-------------------------------------");
+  print(`🥱 Whats are you going to do now? 🥱`);
+  print("-------------------------------------");
+  print("You can see a number of consoles: 🎮");
   CONSOLES.forEach((h, i) => print(`   ${i} - ${h}`));
-  askQuestion("Which console will you choose", chooseConsole);
+  askQuestion("Which console will you choose: 🤔", chooseConsole);
 }
 
 export const CONSOLES = ["PS4", "Xbox", "Nintendo Switch"] as const;
-
 export type Console = (typeof CONSOLES)[number];
+
+const PSGAMES = [
+  "Spiderman",
+  "God of War",
+  "Fortnite",
+  "Horizon Zero Dawn",
+  "Uncharted",
+] as const;
+type PsGames = (typeof PSGAMES)[number];
 
 function chosenConsole(console: Console): void {
   clear(true);
 
   if (console === "PS4") {
-    print(`YAY - You're playing SpiderMan`);
-    return endAdventure();
+    print(
+      "😃😃 YAY - You're playing " +
+        PSGAMES[Math.floor(Math.random() * PSGAMES.length)] +
+        "!!!! 😃😃"
+    );
+    askQuestion(
+      "Press ENTER when you're ready to restart the adventure! ",
+      haveAdventures
+    );
   } else {
-    print(`WHAAAAT ❓🤯😅❓`);
-    print(`You can't find adventures there!`);
-    return endAdventure();
+    return noGaming();
   }
+}
+
+export function noGaming(): void {
+  print(`WHAAAAT ❓🤯😅❓`);
+  print(`You didn't choose PS4 👎😯👎 !!!!`);
+  print(`Best you restart the adventure!`);
+  askQuestion("Press ENTER to restart! ", haveAdventures);
 }
 
 function chooseConsole(input: string) {
@@ -35,7 +55,7 @@ function chooseConsole(input: string) {
   if (console === undefined) {
     print(`😮`);
     print(`${input} is an invalid input 😭`);
-    return endAdventure();
+    return whatsNext();
   }
 
   return chosenConsole(console);
